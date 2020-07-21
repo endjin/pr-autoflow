001bd76c835d8dead9f3db71fd8427e1ae43336d
James Dawson <james.dawson@endjin.com>
2020-07-21 12:28:32 +0100

    Fixed tests for action entrypoint script

--- src/RunAction.Tests.ps1
+++ src/RunAction.Tests.ps1
@@ -2,15 +2,70 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $sutPath = Join-Path $here $sut
 
-# Import-Module $here/module/dependabot-pr-parser.psm1 -DisableNameChecking
+Import-Module $here/module/dependabot-pr-parser.psm1 -DisableNameChecking
 
 Describe 'RunAction Tests' {
 
-    # Mock -ModuleName dependabot-pr-parser SetOutputVariable { throw } -Verifiable
+    Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'dependency_name' }
+    Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'version_from' }
+    Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'version_to' }
+    Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'folder' }
+    
+    Context 'Non-matching package' {
+        Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'is_interesting_package' -and $value -eq $false }
 
-    It 'should run successfully with no package patterns specified' {
-        & $sutPath -Title 'Bump Corvus.Extensions.Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground'
+        It 'should run successfully with no package patterns specified' {
+            & $sutPath -Title 'Bump Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground'
 
-        # Assert-MockCalled -ModuleName dependabot-pr-parser SetOutputVariable -Times 5
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 5
+        }
+
+        It 'should run successfully with a non-matching pattern specified' {
+            & $sutPath -Title 'Bump Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground' -PackageNamePatterns 'Corvus.*'
+
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 5
+        }
+
+        It 'should run successfully with a non-matching JSON-formatted pattern specified' {
+            & $sutPath -Title 'Bump Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground' -PackageNamePatternsJsonArray '["Corvus.*"]'
+
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 5
+        }
+    }
+
+    Context 'Matching package' {
+        Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'is_interesting_package' -and $value -eq $true }
+        Mock SetOutputVariable { } -Verifiable -ParameterFilter { $name -eq 'update_type' }
+
+        It 'should run successfully with a matching pattern specified' {
+            & $sutPath -Title 'Bump Corvus.Extensions.Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground' -PackageNamePatterns 'Corvus.*'
+
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 6
+        }
+
+        It 'should run successfully when matching one of multiple specified patterns' {
+            & $sutPath -Title 'Bump Corvus.Extensions.Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground' -PackageNamePatterns @('Corvus.*', 'Menes.*')
+
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 6
+        }
+
+        It 'should run successfully with a matching JSON-formatted pattern specified' {
+            & $sutPath -Title 'Bump Corvus.Extensions.Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground' -PackageNamePatternsJsonArray '["Corvus.*"]'
+
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 6
+        }
+
+        It 'should run successfully when matching one of multiple specified JSON-formatted patterns' {
+            & $sutPath -Title 'Bump Corvus.Extensions.Newtonsoft.Json from 0.9.0 to 1.0.0 in /Solutions/dependency-playground' -PackageNamePatternsJsonArray '["Corvus.*","Menes.*"]'
+
+            Assert-VerifiableMock
+            Assert-MockCalled SetOutputVariable -Times 6
+        }
     }
 }
