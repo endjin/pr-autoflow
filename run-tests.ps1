2b8423d5fca3fab02510a7b8928e059a509226d3
James Dawson <james.dawson@endjin.com>
2020-07-23 13:36:09 +0100

    Add error handling to get test failures to fail the workflow

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,8 +1,24 @@
 $ErrorActionPreference = 'Stop'
+$here = Split-Path -Parent $PSCommandPath
 $pesterVer = '4.10.1'
-[array]$existingModule = Get-Module -ListAvailable Pester
-if (!$existingModule -or ($existingModule.Version -notcontains $pesterVer)) {
-    Install-Module Pester -RequiredVersion $pesterVer -Force -Scope CurrentUser
+try {
+    [array]$existingModule = Get-Module -ListAvailable Pester
+    if (!$existingModule -or ($existingModule.Version -notcontains $pesterVer)) {
+        Install-Module Pester -RequiredVersion $pesterVer -Force -Scope CurrentUser
+    }
+    Import-Module Pester
+    $results = Invoke-Pester $here/src -PassThru
+
+    if ($results.FailedCount -gt 0) {
+        throw ("{0} out of {1} tests failed - check previous logging for more details" -f $results.FailedCount, $results.TotalCount)
+    }
 }
-Import-Module Pester
-Invoke-Pester ./src
+catch {
+    Write-Output ("::error file={0},line={1},col={2}::{3}" -f `
+                        $_.InvocationInfo.ScriptName,
+                        $_.InvocationInfo.ScriptLineNumber,
+                        $_.InvocationInfo.OffsetInLine,
+                        $_.Exception.Message)
+
+    exit 1
+}
