5ade1a0534a38b8a15ee0f3d5d3e006e193b3b59
James Dawson <james.dawson@endjin.com>
2020-07-31 13:27:12 +0100

    Updated act tests for dependabot-pr-watcher to test for empty PR lists
    act tests are currently failing since the addition of the '#requires' statement - running the act containers manually seem to work, but not during the act test runs?!?

--- src/dependabot-pr-watcher/test-with-act.sh
+++ src/dependabot-pr-watcher/test-with-act.sh
@@ -1,1 +1,2 @@
-act pull_request -e test-workflow-event.json -W . -s GITHUB_TOKEN=foo
+act pull_request -e test-workflow-event.json -j test_job_with_prs -W . -s GITHUB_TOKEN=foo
+act pull_request -e test-workflow-event.json -j test_job_with_no_prs -W . -s GITHUB_TOKEN=foo
